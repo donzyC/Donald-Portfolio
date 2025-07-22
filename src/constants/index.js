0ea1d9c29d990ec60dd720694f4c4021b24e85b8
donzyC <donzy8787@gmail.com>
2025-07-21 22:37:27 -0700

    Update DXAI project live demo link to Render deployment

--- src/constants/index.js
+++ src/constants/index.js
@@ -218,7 +218,7 @@ import {
         ],
         image: dxai,
         source_code_link: "https://github.com/donzyC/DxAi",
-        live_demo_link: "https://dxai.up.railway.app"
+        live_demo_link: "https://dxai.onrender.com"
       },
       {
         name: "Chat Bot",
